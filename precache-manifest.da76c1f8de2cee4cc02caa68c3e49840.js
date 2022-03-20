1191ad469ccd7ac85c6a914dfe39735d08c1fbf3
ptrdom <5850190+ptrdom@users.noreply.github.com>
2022-03-20 10:40:03 +0000

    Deploying to gh-pages from @ ptrdom/infinite-canvas@d861aa42df3e3c778175dba8e7cd5713dd3ca1e1 🚀

---
+++ precache-manifest.da76c1f8de2cee4cc02caa68c3e49840.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4383e0a7dd89fd86d70",
+    "url": "/infinite-canvas/static/js/main.f4383e0a.chunk.js"
+  },
+  {
+    "revision": "889afb290ad9fd325e93",
+    "url": "/infinite-canvas/static/js/1.889afb29.chunk.js"
+  },
+  {
+    "revision": "5756f24206bcb72e8045",
+    "url": "/infinite-canvas/static/js/runtime~main.5756f242.js"
+  },
+  {
+    "revision": "ed6adaa1c6f6930d646d2cf10a2b5d6c",
+    "url": "/infinite-canvas/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/infinite-canvas/precache-manifest.ea7cdb5bd7b6f8a559662d0e98336f1a.js"
+  "/infinite-canvas/precache-manifest.da76c1f8de2cee4cc02caa68c3e49840.js"
 );
 
 workbox.clientsClaim();
